eb9b1fc99d2a462984bc58e46197a2d8732a2197
Moo-ni <86027229+Moo-ni@users.noreply.github.com>
2025-08-18 09:46:44 +0200

    Update run.sh

--- teddycloud/run.sh
+++ teddycloud/run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
-# Start-TeddyCloud-Add-on
-echo "Starte TeddyCloud Server..."
+# Kurze Ausgabe beim Start
+echo "Starte TeddyCloud Add-on..."
+
+# Prüfen, ob die TeddyCloud-Binärdatei existiert
+if [ ! -f /usr/local/bin/teddycloud ]; then
+    echo "Fehler: /usr/local/bin/teddycloud wurde nicht gefunden!"
+    exit 1
+fi
+
+# TeddyCloud-Server starten
 exec /usr/local/bin/teddycloud
